57d7c1d8c6a71d01d50e6da594c1192d7a54f701
YerimSW <swyr1016@gmail.com>
2024-07-23 22:28:16 +0900

    Chore: 프로젝트 생성

---
+++ src/test/java/acc/hotsix/file_share/FileShareApplicationTests.java
@@ -0,0 +1,13 @@
+package acc.hotsix.file_share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
