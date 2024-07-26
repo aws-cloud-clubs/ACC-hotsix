d4ea957ebad9dd87ccd71d43365e9d13b0ac1ec8
mjttong <mjttong@gmail.com>
2024-07-26 16:16:15 +0900

    Feat: 기본 리포지토리와 QueryDSL 리포지토리 생성

---
+++ src/main/java/acc/hotsix/file_share/dao/FileCustomRepository.java
@@ -0,0 +1,4 @@
+package acc.hotsix.file_share.dao;
+
+public interface FileCustomRepository {
+}
---
+++ src/main/java/acc/hotsix/file_share/dao/FileCustomRepositoryImpl.java
@@ -0,0 +1,4 @@
+package acc.hotsix.file_share.dao;
+
+public class FileCustomRepositoryImpl implements FileCustomRepository{
+}
