10311c8e0837ea7793795e2298a5a004935f4853
mjttong <mjttong@gmail.com>
2024-07-26 16:05:12 +0900

    Feat: QueryDSL의 Q클래스 추가

---
+++ src/main/generated/acc/hotsix/file_share/domain/QFile.java
@@ -0,0 +1,55 @@
+package acc.hotsix.file_share.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QFile is a Querydsl query type for File
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QFile extends EntityPathBase<File> {
+
+    private static final long serialVersionUID = -830040864L;
+
+    public static final QFile file = new QFile("file");
+
+    public final DateTimePath<java.time.LocalDateTime> createdAt = createDateTime("createdAt", java.time.LocalDateTime.class);
+
+    public final NumberPath<Long> download = createNumber("download", Long.class);
+
+    public final NumberPath<Long> fileId = createNumber("fileId", Long.class);
+
+    public final NumberPath<Double> fileSize = createNumber("fileSize", Double.class);
+
+    public final StringPath fileType = createString("fileType");
+
+    public final DateTimePath<java.time.LocalDateTime> lastModifiedAt = createDateTime("lastModifiedAt", java.time.LocalDateTime.class);
+
+    public final StringPath name = createString("name");
+
+    public final StringPath path = createString("path");
+
+    public final StringPath resource = createString("resource");
+
+    public final NumberPath<Long> view = createNumber("view", Long.class);
+
+    public QFile(String variable) {
+        super(File.class, forVariable(variable));
+    }
+
+    public QFile(Path<? extends File> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QFile(PathMetadata metadata) {
+        super(File.class, metadata);
+    }
+
+}
+
