074a29295840b9584f71bd5f910e5d3c28fcf8fc
mjttong <mjttong@gmail.com>
2024-07-26 20:37:38 +0900

    Feat: QueryDSL 커스텀 리포지토리 메소드 구현
    - 전체 조회 메소드 구현
    - 페이지 조회 및 정렬 메소드 구현
    - 검색 메소드 구현

--- src/main/java/acc/hotsix/file_share/dto/FileSearchRequestDTO.java
+++ src/main/java/acc/hotsix/file_share/dto/FileSearchRequestDTO.java
@@ -1,5 +1,6 @@
 package acc.hotsix.file_share.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class FileSearchRequestDTO {
     private String name;
     private String path;
