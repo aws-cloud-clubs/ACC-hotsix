a9b9d4c6a7aba9bae2ff889598ba1fb7b90c8365
mjttong <mjttong@gmail.com>
2024-08-15 17:28:53 +0900

    Feat: 삭제 SDK v2 도입

--- src/main/java/acc/hotsix/file_share/application/FileDeleteService.java
+++ src/main/java/acc/hotsix/file_share/application/FileDeleteService.java
@@ -4,17 +4,30 @@ import acc.hotsix.file_share.dao.FileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 
 @Service
 @RequiredArgsConstructor
 public class FileDeleteService {
+
     @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
-    private final FileRepository fileRepository;
-    //private final AmazonS3Client amazonS3Client;
 
+    private final FileRepository fileRepository;
+
+    private final S3Client s3Client;
+
+    @Transactional
     public void deleteFile(Long fileId) {
-       // amazonS3Client.deleteObject(bucketName, fileId.toString());
+        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileId.toString())
+                .build();
+
+        s3Client.deleteObject(deleteObjectRequest);
+
         fileRepository.deleteById(fileId);
     }
 }
