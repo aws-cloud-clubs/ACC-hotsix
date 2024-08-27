711c37b8b58394d62610677f1f0883ecd1d6ce94
mjttong <mjttong@gmail.com>
2024-08-27 14:04:45 +0900

    Fix: credetial을 IAM role을 통해 부여하는 방식으로 변화

--- src/main/java/acc/hotsix/file_share/global/config/S3Config.java
+++ src/main/java/acc/hotsix/file_share/global/config/S3Config.java
@@ -1,41 +1,20 @@
 package acc.hotsix.file_share.global.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
-
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
-
-    @Value("${cloud.aws.region.static}")
-    private String region;
-
-    // access, secret key 이용해 aws 자격증명 제공
-    @Bean
-    public AwsCredentialsProvider awsCredentialsProvider() {
-        AwsCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
-        return StaticCredentialsProvider.create(awsCredentials);
-    }
-
     // s3서비스를 이용하기 위한 S3Client 객체 생성
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(awsCredentialsProvider())
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 
@@ -43,8 +22,8 @@ public class S3Config {
     @Bean
     public S3Presigner s3Presigner() {
         return S3Presigner.builder()
-                .region(Region.of(region))
-                .credentialsProvider(awsCredentialsProvider())
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
